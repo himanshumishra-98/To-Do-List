6fe4eb40467f784e05f254b614651f78ac77d0cc
Himanshu Mishra <himanshu.pmishra555@gmail.com>
2023-09-05 19:23:02 +0530

    Updated app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+let btnInput = document.querySelector(".btn-success");
+let textInput = document.querySelector(".input-txt");
+let taskUL = document.querySelector(".task-list");
+
+function addTask(){
+    let item = document.createElement("li");
+    item.innerText = textInput.value;
+
+    let delBtn = document.createElement("button");
+    delBtn.innerText = "Delete";
+    delBtn.classList.add("btn");
+    delBtn.classList.add("btn-danger");
+
+    item.appendChild(delBtn);
+    taskUL.appendChild(item);
+    textInput.value = "";
+}
+
+textInput.addEventListener("keypress",function(event){
+    if(event.key === "Enter"){
+        event.preventDefault();
+        document.getElementById("myBtn").click();
+    }
+})
+
+taskUL.addEventListener("click",function(event){
+    let delTask = event.target.parentElement;
+    if(event.target.nodeName == "BUTTON"){
+        delTask.remove();
+    }
+});
+
+
+btnInput.addEventListener("click",addTask);
